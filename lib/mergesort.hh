9e0e29ba4e3e9219746904b47b75bb21f2b8c3c5
yvxiang <linyvxiang@gmail.com>
2014-10-21 23:38:42 +0800

    rewrite part of merge sort

--- lib/mergesort.hh
+++ lib/mergesort.hh
@@ -23,38 +23,44 @@ template <typename C, typename F>
 void mergesort_impl(C *a, size_t nmya, size_t afirst, size_t astep, F &pcmp, C &sized_output) {
     typedef typename C::iterator iterator_type;
     xarray<iterator_type> ai;
-    for (size_t i = 0; i < nmya; ++i) {
-        iterator_type mi = a[afirst + i * astep].begin();
-        if (mi != mi.parent_end())
-            ai.push_back(mi);
+    for (size_t i = 0; i < nmya; i++) {
+        if ( a[afirst + i * astep].size() == 0)
+            continue;
+        iterator_type cur_it = a[afirst + i * astep].begin();
+        ai.push_back(cur_it);
     }
-    size_t nsorted = 0;
-    while (nsorted < sized_output.size()) {
-        assert(ai.size());
-	int min_idx = 0;
-	typename C::element_type *min_pair = ai[0].current();
-	for (size_t i = 1; i < ai.size(); ++i)
-	    if (pcmp(min_pair, ai[i].current()) > 0) {
-		min_pair = ai[i].current();
-		min_idx = i;
-	    }
-        sized_output[nsorted ++] = *min_pair;
-        ++ai[min_idx];
-        if (ai[min_idx] == ai[min_idx].parent_end())
-            ai.remove(min_idx);
+
+    size_t sorted_pairs = 0;
+    while (sorted_pairs < sized_output.size()) {
+        size_t min_index = 0, cur_index;
+        for (; ai[min_index] == ai[min_index].parent_end(); min_index++)
+            ;
+        for (cur_index = min_index; cur_index < ai.size(); cur_index++)
+            if (pcmp(ai[min_index].current(), ai[cur_index].current()) > 0)
+                min_index = cur_index;
+
+        sized_output[sorted_pairs++] = *ai[min_index];
+        ai[min_index]++;
+        //if(ai[min_index].current() == *(ai[min_index].parent_end()))
+        if (ai[min_index] == ai[min_index].parent_end())
+            ai.remove(min_index);
+
     }
+
     assert(!ai.size());
 }
 
 template <typename C, typename F>
 C *mergesort(xarray<C> &a, size_t astep, size_t afirst, F &pcmp) {
-    size_t nmya = a.size() / astep + (size_t(afirst) < (a.size() % astep));
+    size_t nmya = a.size() / astep + (afirst < (a.size() % astep));;
+    //number of pairs to sort
     size_t np = 0;
     for (size_t i = 0; i < nmya; i++)
-	np += a[afirst + i * astep].size();
+        np += a[afirst + i * astep].size();
     C *out = new C(np);
     if (np == 0)
-	return out;
+        return out;
+
     mergesort_impl(a.array(), nmya, afirst, astep, pcmp, *out);
     dprintf("mergesort: afirst %zd astep %zd (collections %zd : nr-kvs %zu)\n",
  	    afirst, astep, a.size(), np);
